c21164d833b7ac30d690b691eb82e612e5aa014f
Herick2D <contato.herickmoreira@gmail.com>
2025-04-07 21:59:15 -0300

    feat: adição do método post para criar tasks

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-const connectToDatabase = require('./src/database/mongoose.database.js');
+const connectToDatabase = require('./src/database/mongoose.database');
+const TaskModel = require('./src/models/task.model');
 
 dotenv.config();
 const app = express();
+app.use(express.json()); // infere que todos os req's serão JSON e por isso não é necessário fazer o JSON.PARSE ou qualquer tipo de conversão
 
 connectToDatabase();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Olá Mundo!');
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
+
+app.post('/tasks', async (req, res) => {
+    const newTask = new TaskModel(req.body);
+    await newTask.save();
+    res.status(201).send(newTask);
+})
 
 app.listen(8000, () => console.log('Escutando na porta 8000!'));
