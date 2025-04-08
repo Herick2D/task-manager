2c1a2fd6407c2b950b501f30dee2d16554edea10
Herick2D <contato.herickmoreira@gmail.com>
2025-04-07 21:06:22 -0300

    feat: adicionando task model

---
+++ src/models/task.model.js
@@ -0,0 +1,17 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const TaskSchema = new Schema({
+    descricao: {
+        type: String,
+        required: true,
+    },
+    estaCompleto: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+const TaskModel = mongoose.model('Tasks', TaskSchema);
+
+module.exports = TaskModel;
